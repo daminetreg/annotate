528c0100dbdbfcd2adbf92364ed1a3f2c1555418
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2016-12-27 23:20:41 +0100

    FEATURE: Made full example of config ser/deser.

--- ecolink510.cpp
+++ ecolink510.cpp
@@ -116,26 +116,26 @@ struct alignas(1) bo_polarities_t {
   
   uint8_t reserved : 2; //XXX: Support writing 2_bits; ?
 
-  bool triac_01_polarity : 1; 
-  bool triac_03_polarity : 1; 
-  bool triac_05_polarity : 1; 
+  bool triac_01 : 1; 
+  bool triac_03 : 1; 
+  bool triac_05 : 1; 
 
-  bool relay_25_polarity : 1; 
-  bool relay_26_polarity : 1; 
-  bool relay_27_polarity : 1; 
+  bool relay_25 : 1; 
+  bool relay_26 : 1; 
+  bool relay_27 : 1; 
 };
 
 struct alignas(1) bo_safety_values_t {
   
   uint8_t reserved : 2; //XXX: Support writing 2_bits; ?
 
-  bool triac_01_safety_value : 1; 
-  bool triac_03_safety_value : 1; 
-  bool triac_05_safety_value : 1; 
+  bool triac_01 : 1; 
+  bool triac_03 : 1; 
+  bool triac_05 : 1; 
 
-  bool relay_25_safety_value : 1; 
-  bool relay_26_safety_value : 1; 
-  bool relay_27_safety_value : 1; 
+  bool relay_25 : 1; 
+  bool relay_26 : 1; 
+  bool relay_27 : 1; 
 };
 
 
@@ -153,6 +153,99 @@ struct alignas(1) bi_polarities_t {
 };
 
 struct em510_binary_representation {
+
+  em510_binary_representation() {}
+  em510_binary_representation(config::ey_em510fxx& src) {
+    triac_01_pulse_duration = src.triac_01.pulse_duration.count();
+    triac_03_pulse_duration = src.triac_03.pulse_duration.count();
+    triac_05_pulse_duration = src.triac_05.pulse_duration.count();
+
+    relay_25_pulse_duration = src.relay_25.pulse_duration.count();
+    relay_26_pulse_duration = src.relay_26.pulse_duration.count();
+    relay_27_pulse_duration = src.relay_27.pulse_duration.count();
+
+    { bo_polarities_t p;
+      p.triac_01 = src.triac_01.polarity;
+      p.triac_03 = src.triac_03.polarity;
+      p.triac_05 = src.triac_05.polarity;
+
+      p.relay_25 = src.relay_25.polarity;
+      p.relay_26 = src.relay_26.polarity;
+      p.relay_27 = src.relay_27.polarity;
+      bo_polarities = p;
+    }
+
+    { bi_polarities_t p;
+      p.ai_18 = src.ai_18;
+      p.ai_20 = src.ai_20;
+      p.ai_22 = src.ai_22;
+      p.ai_23 = src.ai_23;
+      bi_polarities = p;
+    }
+
+    ao_07_safety_value = src.ao_07;
+    ao_09_safety_value = src.ao_09;
+    ao_11_safety_value = src.ao_11;
+
+    { bo_safety_values_t s;
+      s.triac_01 = src.triac_01.safety_value;
+      s.triac_03 = src.triac_03.safety_value;
+      s.triac_05 = src.triac_05.safety_value;
+
+      s.relay_25 = src.relay_25.safety_value;
+      s.relay_26 = src.relay_26.safety_value;
+      s.relay_27 = src.relay_27.safety_value;
+
+      bo_safety_values = s;
+    }
+
+  }
+
+  operator config::ey_em510fxx () {
+    config::ey_em510fxx dst;
+
+    dst.triac_01.pulse_duration = std::chrono::milliseconds{triac_01_pulse_duration.value()};
+    dst.triac_03.pulse_duration = std::chrono::milliseconds{triac_03_pulse_duration.value()};
+    dst.triac_05.pulse_duration = std::chrono::milliseconds{triac_05_pulse_duration.value()};
+
+    dst.relay_25.pulse_duration = std::chrono::milliseconds{relay_25_pulse_duration.value()};
+    dst.relay_26.pulse_duration = std::chrono::milliseconds{relay_26_pulse_duration.value()};
+    dst.relay_27.pulse_duration = std::chrono::milliseconds{relay_27_pulse_duration.value()};
+
+    { bo_polarities_t p = bo_polarities.value();
+       dst.triac_01.polarity = p.triac_01;
+       dst.triac_03.polarity = p.triac_03;
+       dst.triac_05.polarity = p.triac_05;
+
+       dst.relay_25.polarity = p.relay_25;
+       dst.relay_26.polarity = p.relay_26;
+       dst.relay_27.polarity = p.relay_27;
+    }
+
+    { bi_polarities_t p = bi_polarities.value();
+      dst.ai_18 = p.ai_18; 
+      dst.ai_20 = p.ai_20; 
+      dst.ai_22 = p.ai_22; 
+      dst.ai_23 = p.ai_23; 
+    }
+
+    dst.ao_07 = ao_07_safety_value.value();
+    dst.ao_09 = ao_09_safety_value.value();
+    dst.ao_11 = ao_11_safety_value.value();
+
+    { bo_safety_values_t s;
+
+      dst.triac_01.safety_value = s.triac_01;
+      dst.triac_03.safety_value = s.triac_03;
+      dst.triac_05.safety_value = s.triac_05;
+      dst.relay_25.safety_value = s.relay_25;
+      dst.relay_26.safety_value = s.relay_26;
+      dst.relay_27.safety_value = s.relay_27;
+    }
+
+    return dst;
+  }
+
   big_int8_buf_t triac_01_pulse_duration;
   big_int8_buf_t triac_03_pulse_duration;
   big_int8_buf_t triac_05_pulse_duration;
@@ -200,6 +293,35 @@ namespace endian
 
     };
 
+    template<>
+    struct unrolled_byte_loops<bi_polarities_t, 1, false>
+    {
+      typedef bi_polarities_t T;
+      static T load_big(const unsigned char* bytes) BOOST_NOEXCEPT
+        { return *reinterpret_cast<const T*>(bytes - 1); }
+      static T load_little(const unsigned char* bytes) BOOST_NOEXCEPT
+        { return *reinterpret_cast<const T*>(bytes); }
+      static void store_big(char* bytes, T value) BOOST_NOEXCEPT
+        { *(bytes - 1) = *(reinterpret_cast<char*>(&value)); }
+      static void store_little(char* bytes, T value) BOOST_NOEXCEPT
+        { *bytes = *(reinterpret_cast<char*>(&value)); }
+
+    };
+
+    template<>
+    struct unrolled_byte_loops<bo_safety_values_t, 1, false>
+    {
+      typedef bo_safety_values_t T;
+      static T load_big(const unsigned char* bytes) BOOST_NOEXCEPT
+        { return *reinterpret_cast<const T*>(bytes - 1); }
+      static T load_little(const unsigned char* bytes) BOOST_NOEXCEPT
+        { return *reinterpret_cast<const T*>(bytes); }
+      static void store_big(char* bytes, T value) BOOST_NOEXCEPT
+        { *(bytes - 1) = *(reinterpret_cast<char*>(&value)); }
+      static void store_little(char* bytes, T value) BOOST_NOEXCEPT
+        { *bytes = *(reinterpret_cast<char*>(&value)); }
+
+    };
   }
 }
 }
@@ -207,18 +329,31 @@ namespace endian
 
 int main(int, char* [])
 {
-  em510_binary_representation h;
-  h.triac_01_pulse_duration = 12;
-  bo_polarities_t pols;
-  pols.triac_01_polarity = true;
-  pols.relay_26_polarity = true;
-  pols.relay_27_polarity = true;
-  h.bo_polarities = pols; //XXX: THis is the main problem to solve first.
+
+  config::ey_em510fxx mycfg;
+  mycfg.triac_01.safety_value = true;
+  mycfg.triac_03.polarity = true;
+  mycfg.ai_23 = true;
+  //mycfg.slc_timeout = std::chrono::seconds{15}; 
+
+  em510_binary_representation h{mycfg};
 
   std::string buffer (sizeof(h), char{});
   std::memcpy(const_cast<char*>(buffer.data()), &h, sizeof(h));
 
   std::cout << sizeof(h) << "-" << buffer.size() << " - " << pre::bytes::to_hexstring(buffer) << std::endl;
+
+  em510_binary_representation deser{mycfg};
+  std::memcpy(&deser, buffer.data(), sizeof(h));
+  
+  config::ey_em510fxx desered = deser;
+
+  assert(desered.triac_01.safety_value == mycfg.triac_01.safety_value);
+  assert(desered.triac_03.polarity == mycfg.triac_03.polarity);
+  assert(desered.ai_23 == mycfg.ai_23);
+  //assert(desered.slc_timeout == mycfg.slc_timeout);
+
+
 
   //  Low-level I/O such as POSIX read/write or <cstdio>
   //  fread/fwrite is sometimes used for binary file operations
